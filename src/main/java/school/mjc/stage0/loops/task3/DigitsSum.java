08b37734d2ea3a87de85a6dbfffee53ca0e5a29c
admin <nbisametova@mail.ru>
2022-11-21 00:05:54 +0600

    2nd try

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -1,10 +1,11 @@
 package school.mjc.stage0.loops.task3;
+import java.lang.Math;
 
 public class DigitsSum {
     public void printDigitsSum(int t) {
         String s;
         int sum = 0;
-        s = "" + t;
+        s = "" + Math.abs(t);
         for (int i = 0; i < s.length(); i++) {
             sum += Integer.parseInt(String.valueOf(s.charAt(i)));
         }
--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,12 +2,14 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
-        int  sum, right =1, left = 1;
-        for (int i = 0; i<lastFibonacci;i++){
+        int sum, right =1, left = 0;
+        System.out.println(left);
+
+        for (int i = 1; i<lastFibonacci;i++){
+            System.out.println(right);
             sum = left +right;
             left = right;
             right = sum;
-            System.out.println(right);
         }
     }
 }
